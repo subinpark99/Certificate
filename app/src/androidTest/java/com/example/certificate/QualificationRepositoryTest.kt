8710f6e1e5e8cca53c79c793b197f143887c6db5
subinpark99 <psb8909@naver.com>
2025-01-21 18:44:57 +0900

    테스트 삭제(오류)

--- app/src/androidTest/java/com/example/certificate/QualificationRepositoryTest.kt
+++ app/src/androidTest/java/com/example/certificate/QualificationRepositoryTest.kt
@@ -16,24 +16,5 @@ class QualificationRepositoryTest {
     @get:Rule
     val hiltRule = HiltAndroidRule(this)
 
-    @Inject
-    lateinit var repository: QualificationRepository
 
-    @Before
-    fun setup() {
-        hiltRule.inject() // Hilt 주입
-    }
-
-    @Test
-    fun fetchAllQualificationData_returnsCorrectData() = runBlocking {
-        val year = 2023
-        val itemName = "정보처리기사"
-
-        val result = repository.fetchAllQualificationData(
-            acquYy = year,
-            jmNm = itemName
-        )
-
-        assertTrue(result.isNotEmpty()) // 결과 데이터가 비어있지 않음을 확인
-    }
 }
--- app/src/androidTest/java/com/example/certificate/QualificationViewModelTest.kt
+++ app/src/androidTest/java/com/example/certificate/QualificationViewModelTest.kt
@@ -16,45 +16,5 @@ import javax.inject.Inject
 @HiltAndroidTest
 class QualificationViewModelTest {
 
-    @get:Rule
-    val hiltRule = HiltAndroidRule(this)
 
-    @Inject
-    lateinit var repository: QualificationRepository // Repository를 주입
-
-    //lateinit var viewModel: QualificationViewModel
-
-    @Before
-    fun setup() {
-        hiltRule.inject() // Hilt 주입
-
-        // viewModel = QualificationViewModel(repository)
-    }
-
-    @Test
-    fun fetchAllQualificationData_returnsCorrectData() = runBlocking {
-        val year = 2023
-        val itemName = "정보처리기사"
-
-        val result = repository.fetchAllQualificationData(
-            acquYy = year,
-            jmNm = itemName
-        )
-
-        assertTrue(result.isNotEmpty()) // 결과 데이터가 비어있지 않음을 확인
-    }
-    
-    @Test
-    fun fetchQualifications_updatesUiStateSuccessfully() = runTest {
-
-        val year = 2023
-        val itemName = "정보처리기사"
-
-        val viewmodel = QualificationViewModel(repository)
-
-        viewmodel.getQualificationData(acquYy = year, jmNm = itemName)
-        assertTrue(viewmodel.uiState.value is UiState.Success)
-
-
-    }
 }
